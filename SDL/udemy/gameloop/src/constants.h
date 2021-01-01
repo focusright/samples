a32ad2394ab465580577f0fec10bbc713bce9876
Mike Lee <rockespecialee@gmail.com>
2020-12-31 19:23:42 -0800

    add udemy SDL game loop example

---
+++ SDL/udemy/gameloop/src/constants.h
@@ -0,0 +1,12 @@
+//https://www.udemy.com/course/game-loop-c-sdl/learn/lecture/20478049#overview
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define FPS 60
+#define FRAME_TARGET_TIME (1000 / FPS)
+
+#endif
